88b686b6c30f9dd1ca7b2712071b96cb10b75c1a
Fedenistov Dmitriy Vyacheslavovich <fdmitriy@inbox.ru>
2025-06-06 23:28:05 +0300

    Added output at two languages.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Привет мир!");
         System.out.print("Hello world!");
-        System.out.println();
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
